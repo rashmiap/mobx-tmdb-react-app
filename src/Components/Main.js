c5908f982a6c13dd0d86f4449d2a49b109f61b48
rashmiap <rashmiap.10@gmail.com>
2018-12-05 14:40:54 +0530

    movie listing added in routes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,11 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import Counter from './Counter';
 import Todo from './Todo';
 import NoMatch from './NoMatch';
+import MovieList from './MovieList';
 
 const Main = (props) => {
   return <main className="Movie-content">
     <Switch>
-      <Route exact path='/' render={() => <Todo/>} />
+      <Route exact path='/' render={() => <MovieList />} />
       <Route path='/movies' render={() => <Counter {...props} />} />
       <Route path='/shows' render={() => <Todo {...props}/>} />
       <Route component={NoMatch} />
