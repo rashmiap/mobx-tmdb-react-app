e5931d68c9fbcf869a8dd563f0285d577e8867be
rashmiap <rashmiap.10@gmail.com>
2018-12-05 14:51:21 +0530

    movieStore store created has loader /isError observables added

---
+++ src/stores/MovieStore.js
@@ -0,0 +1,34 @@
+import { observable, action, reaction, autorun, computed } from 'mobx';
+import Movie from './Movie';
+
+export class MovieStore {
+    @observable movies = [];
+    @observable isLoading = false;
+    @observable isError = false;
+    @observable filter = "";
+
+    fetchMovies(){
+      this.isLoading = true;
+      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=28967d69513d49d94603253876b995a8&language=en-US&page=1`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then((response) => {
+        this.isLoading = false;
+        response.results.map(item => {
+          this.movies.push(new Movie(item));
+        });
+      })
+      .catch(error => {
+        this.isLoading = false;
+        this.isError = true;
+        console.log("Error is fetching movies", error)
+      });
+    }
+}
+
+export default new MovieStore();
