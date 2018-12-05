63827df48d5862f9b099f9958171e93519724111
rashmiap <rashmiap.10@gmail.com>
2018-12-05 11:14:35 +0530

    initial project setup cloned from my other mobx-react-app repo
    create-react-app rewired to avoid inject

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# mobx-tmdb-react-app
+[**MobX-React-App**](https://github.com/rashmiap/mobx-react-app)
+-
+Playing around with [MobX](https://github.com/mobxjs/mobx)
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+Screenshots 
+-
+![Preview Movie](src/preview-screen1.png)
+![Preview Shows](src/preview-screen2.png)
+
+**Run Locally**
+-
+ 1.  Clone this repository and go into its src folder.
+ 2.  Run  `npm install`
+ 3.  Run  `npm start`
+ 4.  Go to  `http://localhost:3000`  to view the app
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { inject, observer } from 'mobx-react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Todo from './Components/Todo';
+import Counter from './Components/Counter';
+import NoMatch from './Components/NoMatch';
+import DevTools from 'mobx-react-devtools';
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <DevTools/>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Todo} />
+            <Route exact path="/counter" component={Counter} />
+            <Route exact path="/todo" component={Todo} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </section>
+    );
+  }
+}
+
+export default App;
