3d4ab25e612bc18c0afd3a19411c1a1fbf731d22
rashmiap <rashmiap.10@gmail.com>
2018-12-06 13:52:51 +0530

    material icon link added Navigation link changed saved observable added

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -12,10 +12,6 @@ const Navigation = (props) => {
         <img src={movieNav} width={35} alt='movie logo'/>
         <p className="Nav-text Nav-movies">Upcoming Movies</p>
       </NavLink>
-      <NavLink activeClassName="Nav-active" to="/shows">
-        <img src={showNav} width={35} alt='shows logo'/>
-        <p className="Nav-text Nav-shows">TV Shows</p>
-      </NavLink>
       <NavLink activeClassName="Nav-active" to="/saved">
         <img src={savedNav} width={35} alt='saved logo'/>
         <p className="Nav-text Nav-saved">Saved Cards</p>
