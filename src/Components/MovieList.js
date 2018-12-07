f5c58502f79f4dddf30680d34b2d6f4b426f7465
rashmiap <rashmiap.10@gmail.com>
2018-12-07 15:21:25 +0530

    added search filter key in movie listing page

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import MovieTile from './MovieTile';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
 
 @inject('MovieStore')
 @observer class MovieList extends Component {
@@ -14,17 +15,20 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
   renderTiles(){
     let renderBlock = [];
-    renderBlock = this.props.MovieStore.movies !== undefined ?
-     this.props.MovieStore.movies.map((key, index) => {
+    renderBlock = this.props.MovieStore.filteredMovie !== undefined ?
+     this.props.MovieStore.filteredMovie.map((key, index) => {
        return (
-         <MovieTile key={index} {...this.props} movies={this.props.MovieStore.movies} i={index} />
+         <MovieTile key={index} {...this.props} movies={this.props.MovieStore.filteredMovie} i={index} />
        );
      })
      : [];
     return renderBlock;
   }
+  handleFilter(e){
+    this.props.MovieStore.filter = e.target.value;
+  }
   render() {
-    const { movies, isLoading, isError } = this.props.MovieStore;
+    const { movies, isLoading, isError, filter } = this.props.MovieStore;
 
     if (isError) {
       return <div className="movie-listing__error">
@@ -39,7 +43,10 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
     return (
       <div className="movie-listing">
-        <h1>Upcoming movies</h1>
+        <div className="movie-listing__filter">
+          <h1>Upcoming movies</h1>
+          <TextField id="movie-filter" variant="outlined" value={filter} onChange={this.handleFilter.bind(this)}  placeholder={'Search for a movie'}/>
+        </div>
         <div className="movie-listing__blocks">
           {this.renderTiles()}
         </div>
